210ef929830089f9fef6b474590c5eafdc1b47d4
winson-ye <wye001@gmail.com>
2021-08-19 23:57:40 -0700

    changes to sign in

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import React, { useState } from 'react';
+import { Avatar, Button, TextField, Link, Grid } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { Paper } from '@material-ui/core';
 
 function Copyright() {
   return (
@@ -32,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    padding: theme.spacing(2)
   },
   avatar: {
     margin: theme.spacing(1),
@@ -50,13 +44,12 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
+    <Container maxWidth="xs">
+      <Paper className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography variant="h5">
           Sign in
         </Typography>
         <form className={classes.form} noValidate>
@@ -82,10 +75,6 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -95,10 +84,10 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
+          <Grid container justifyContent="space-between">
+            <Grid item>
               <Link href="#" variant="body2">
-                Forgot password?
+                {"Forget password?"}
               </Link>
             </Grid>
             <Grid item>
@@ -108,10 +97,7 @@ export default function SignIn() {
             </Grid>
           </Grid>
         </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+      </Paper>
     </Container>
   );
 }
