952160cfdf82600b2e9c2cfca6ea2a5027f2867d
winson-ye <wye001@gmail.com>
2021-08-26 22:36:24 -0700

    minor style changes

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
@@ -10,15 +10,10 @@ import useStyles from './styles';
 const Navbar = ({ user, setUser }) => {
   const dispatch = useDispatch();
   const classes = useStyles();
-  const history = useHistory();
 
   const logout = () => {
     dispatch({ type: actionType.LOGOUT });
     setUser(null);
-  };
-
-  const jumpToAnimeList = () => {
-    history.push('/animelist');
   };
 
   useEffect(() => {
@@ -44,7 +39,7 @@ const Navbar = ({ user, setUser }) => {
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.firstName.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{`${user?.result.firstName} ${user?.result.lastName}`}</Typography>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-            <Button variant="contained" color="secondary" onClick={jumpToAnimeList}>My Anime List</Button>
+            <Button variant="contained" color="secondary" href="/animelist">My Anime List</Button>
           </div>
         ) : (
           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
