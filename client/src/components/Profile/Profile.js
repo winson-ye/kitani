636d4794cde8fd16667b84de0f3236d84210dd2f
winson-ye <wye001@gmail.com>
2021-09-05 04:49:05 -0700

    minor additions to profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,10 +1,14 @@
 import AnimeList from '../AnimeList/AnimeList';
+import FollowingCard from '../FollowingCard/FollowingCard';
 import { Grid } from '@material-ui/core';
 
 const Profile = () => {
     return (
         <Grid container direction="column" alignItems="center">
             <AnimeList />
+            <Grid item>
+                <FollowingCard />
+            </Grid>
         </Grid>
     );
 };
