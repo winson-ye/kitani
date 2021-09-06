5e1f156cb64fef64f0d59cb8ebbd4a7118d7e912
winson-ye <wye001@gmail.com>
2021-09-06 13:03:23 -0700

    Multiple users able to add anime lists now

--- client/src/components/AnimeList/AnimeList.js
+++ client/src/components/AnimeList/AnimeList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Avatar, List, Grid, ListItem, ListItemAvatar, ListItemText, CircularProgress } from '@material-ui/core';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import useStyles from './styles';
@@ -12,41 +12,32 @@ const AnimeList = () => {
   const dispatch = useDispatch();
 
   const user = JSON.parse(localStorage.getItem('profile'));
-
   const animeList = useSelector((state) => (state.animeList.creator === user?.result?._id ? state.animeList : { creator: '', shows: [] }));
-
-  const [animeListOrdered, updateAnimeListOrdered] = useState(animeList.shows);
 
   useEffect(() => {
     dispatch(getAnime(user?.result?._id));
   }, [dispatch, user?.result?._id]);
 
-  useEffect(() => {
-    if (animeList.length !== 0) {
-      updateAnimeListOrdered(animeList.shows);
-    }
-  }, [animeList]);
-
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
 
-    const items = Array.from(animeListOrdered);
+    const items = Array.from(animeList.shows);
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
 
+    animeList.shows = items;
     dispatch(updateAnimeList(user?.result?._id, items));
-    updateAnimeListOrdered(items);
   }
 
   return (
-    !animeListOrdered.length ? <CircularProgress /> : (
+    !animeList.shows.length ? <CircularProgress /> : (
       <Grid item className={classes.list}>
         <DragDropContext onDragEnd={handleOnDragEnd}>
           <Droppable droppableId="anime">
             {(provided) => (
               <List {...provided.droppableProps} ref={provided.innerRef}>
               {
-              animeListOrdered.map((value, index) => {
+              animeList.shows.map((value, index) => {
                 return (
                   <Draggable key={value.showId} draggableId={value.showId} index={index}>
                     {(provided) => (
