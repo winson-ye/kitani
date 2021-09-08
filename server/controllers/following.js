c5bfc11a470cf19b8c70f70e9b60c511e26fdf7a
winson-ye <wye001@gmail.com>
2021-09-08 02:05:42 -0700

    Finished backend error with following same person twice

--- server/controllers/following.js
+++ server/controllers/following.js
@@ -27,8 +27,13 @@ export const addFollowee = async (req, res) => {
         await newFolloweeList.save();
         res.status(201).json(newFolloweeList);
     } else {
-        followeeList.followees.push(req.body);
-        const updatedFolloweeList = await Following.findOneAndUpdate({ creator: id }, { followees: followeeList.followees }, { new: true });
-        res.status(200).json(updatedFolloweeList);
+        if (followeeList.followees.find((element) => element.followeeId === req.body.followeeId)) {
+            const newFolloweeList = new Following({ follower: id, followees: req.body });
+            res.status(400).json(newFolloweeList);
+        } else {
+            followeeList.followees.push(req.body);
+            const updatedFolloweeList = await Following.findOneAndUpdate({ creator: id }, { followees: followeeList.followees }, { new: true });
+            res.status(200).json(updatedFolloweeList);
+        }
     }
 }
